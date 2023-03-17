a9b910daccd86621af96ca832d52ff4789cf1651
Mar <mar@Inception.inception.virtualbox.org>
2023-03-17 18:07:29 +0000

    start

---
+++ wp_config_sample.php
@@ -0,0 +1,19 @@
+// ** MYSQL settings - You can get this info from your web host ** //
+
+//** The name of the database for WordPress */
+define ( 'DB_NAME', 'database_name_here' );
+
+/** MySQL database username */
+define( 'DB_USER', 'username_here' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'password_here' );
+
+/** MYSQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+
+/** RESOURCES
+ *  github.com/WordPress/WordPress/blob/master/wp-config-sample.php
+ *  developer.wordpress.org/apis/wp-config-php/
+ */
